13e9d34d30fc48dd6550098b779916b720b4b381
Luigi-PastorePica <luigi.pastorepica@outlook.com>
2016-03-02 16:02:48 -0500

    Gets Links From First Response
    Obtains relevant link tags from first response, which were a bit difficult to get at first because of some odd formatting on the website. Next comes accessing the links and scraping the required info

--- Tuft/scraper_tuft.py
+++ Tuft/scraper_tuft.py
@@ -7,13 +7,14 @@ from time import sleep
 from random import seed
 from random import randint
 import re
-from scrape_util import *
+from scrape_util import *       # Is it preferable to just import the module and call funct. in this way: scrape_util.instantiate_browser()
 from person_class import *
 
 url = 'http://directory.tufts.edu/asearch.cgi'
 first_char = 'a'
 last_char = 'a'
 
+# Instantiation of a browser
 browser_obj = instantiate_browser(url, False)
 browser_obj.select_form("search")
 
@@ -21,41 +22,60 @@ query_chars = char_list_generator(first_char, last_char)
 
 # start a for loop here to iterate over query_chars
 
-# This portion, which fills the appropriate fields in the directory form, might become a function in the future.
-# For that purpose browser object and query_char would be the arguments
-browser_obj['type'] = ['Students']
-browser_obj['LastOption'] = ['starts']
-browser_obj['getLast'] = query_chars[0]     # Temporarily using only one set of chars for development and debugging
+# This function will eventually be moved to scrape_util module
+# Fills directory form and submits it. Returns the response from the mechanize browser object
+# Fields to be filled and values are mostly predetermined. Only query_char (second argument) changes when called.
+def fill_form(browser_object, query_string):
+    browser_object['type'] = ['Students']
+    browser_object['LastOption'] = ['starts']
+    browser_object['getLast'] = query_string     # Temporarily using only one set of chars for development and debugging
 
 
-# This piece of code below does exactly the same
-# control1 = browser_obj.form.find_control('type')
-# control1.value = ['Students']
-# control2 = browser_obj.form.find_control('LastOption')
-# control2.value = ['starts']
-# control3 = browser_obj.form.find_control('getLast')
-# control3.value = query_char
+    # This piece of code below does exactly the same
 
-# And yet again, the code below does exactly the same
-# control1 = browser_obj.form.find_control('type')
-# browser_obj[control1.name] = ['Students']
-# control2 = browser_obj.form.find_control('LastOption')
-# browser_obj[control2.name] = ['starts']
-# control3 = browser_obj.form.find_control('getLast')
-# browser_obj[control3.name] = query_char
+    # control1 = browser_obj.form.find_control('type')
+    # control1.value = ['Students']
+    # control2 = browser_obj.form.find_control('LastOption')
+    # control2.value = ['starts']
+    # control3 = browser_obj.form.find_control('getLast')
+    # control3.value = query_char
 
-browser_obj.submit()
+    # And yet again, the code below does exactly the same
+    
+    # control1 = browser_obj.form.find_control('type')
+    # browser_obj[control1.name] = ['Students']
+    # control2 = browser_obj.form.find_control('LastOption')
+    # browser_obj[control2.name] = ['starts']
+    # control3 = browser_obj.form.find_control('getLast')
+    # browser_obj[control3.name] = query_char
 
-# Next, checking the response and using a loop to go through all the links provided.
+    browser_object.submit()
+    return browser_object.response()
+
+soup = BeautifulSoup(fill_form(browser_obj, query_chars[0]).read(), "html.parser")  # Testing with only one query_string
+
+soup.html.extract()  # Removes the whole html tag. Only what is after the html tag is required. Returned object lost.
+
+link_tags = soup.find_all('a')  # Gets all the link tags
+del link_tags[-1]               # Last link removed, not relevant.
+
+for link_tag in link_tags:
+    full_name = link_tag.string.extract()
+    print full_name
+    # link_tag.a.unwrap()
+    print
+
+
+for link_tag in link_tags:
+    print link_tag
+    print
+
+
+
 # Each link will be opened and BeautifulSoup will be used to get all relevant info from response.
 # BeautifulSoup object will be overwritten on each loop.
 # Once all links checked, go back to first browser object and enter new chars into form.
 
-
-
-# This part is used only for getting html in a file. Will be removed eventually.
-# with open('tuft_page_response.txt', 'wb') as f:
-#     f.write(browser_obj.response().read())
 
 
 
